09a5a9d176803a14cde94fe2e77fd3879a9d16d2
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-05-28 22:21:18 -0400

    removed unnecessary files

--- edu.cmu.ds.lab1/src/network/Server.java
+++ edu.cmu.ds.lab1/src/network/Server.java
@@ -1,5 +1,10 @@
 package network;
 
 public class Server {
-
+	public static int initialPort = 2222;
+	
+	//get initial server port for communication
+	public int getInitialPort(){
+		return initialPort;
+	}
 }
