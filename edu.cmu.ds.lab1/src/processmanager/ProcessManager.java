0e3681113cdb4f89d34891275535c2add3c6623e
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-05-30 02:50:32 -0400

    added class diagrams

--- edu.cmu.ds.lab1/src/processmanager/ProcessManager.java
+++ edu.cmu.ds.lab1/src/processmanager/ProcessManager.java
@@ -12,23 +12,13 @@ import network.Server;
 public class ProcessManager {
 	
 	public static void main(String args[]){
-		int portNumber = Server.INITIAL_PORT;
-        while(true){
-		        try (
-		            ServerSocket serverSocket = new ServerSocket(Server.INITIAL_PORT);
-		            Socket clientSocket = serverSocket.accept();     
-		            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
-		            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		        ) {
-		            String inputLine;
-		            while ((inputLine = in.readLine()) != null) {
-		                out.println(inputLine);
-		            }
-		        } catch (IOException e) {
-		            System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
-		            System.out.println(e.getMessage());
-		        }
-        }
+		log("Start server.java");
+		Server.displayClients();
+		log("Start server.java");
+	}
+	
+	static void log(String a){
+		System.out.println(a);
 	}
 
 }
